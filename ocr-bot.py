121a54ae7bf7446dfdc1153e3d533f3884b5f625
Luís Chaves <pi.luis.f3@usp.br>
2021-08-11 11:11:46 -0300

    Improvements on typehint for better intellisense. Filename of resulting ocr file now is the same as the input file

--- ocr-bot.py
+++ ocr-bot.py
@@ -4,9 +4,8 @@ import os
 import asyncio
 
 import ocrmypdf
-import nanoid
 
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, custom
 
 def log_config():
     formatter_str = '%(asctime)s [%(levelname)s] %(name)s (%(lineno)s): %(message)s'
@@ -67,35 +66,39 @@ def main():
     with TelegramClient('name', api_id, api_hash).start(bot_token=bot_token) as client:
         @client.on(events.NewMessage(incoming=True, pattern='/start'))
         async def start_command(event: events.newmessage.NewMessage.Event):
-            await event.respond('Olá! Meu nome é Pytonisia e posso transformar pdfs em pdfs acessíveis/pesquisáveis')
-            await event.respond('Tenha em mente que pdfs grandes podem demorar para serem processados. Se já possuirem algum OCR, vai demorar um pouco mais, pois será necessário limpar o OCR anterior.')
-            await event.respond('Para definir a(s) língua(s) do documento, utilize o comando -l lang1+lang2+lang3 no texto da mensagem do documento')
-            await event.respond('No momento, são suportas as línguas português (por), inglês (eng) e espanhol (spa)')
-            await event.respond('Exemplo de comando: `-l por+eng`')
-            await event.respond('O código fonte pode ser encontrado em https://github.com/LuisF3')
-            await event.respond('Se quiser, você pode doar uma quantia pelo pix. A chave aleatória é: 5edf6e87-8c5b-4cb9-b584-6ec1f12c8cbe')
+            message_obj: custom.message.Message = event.message
+
+            await message_obj.respond('Olá! Meu nome é Pytonisia e posso transformar pdfs em pdfs acessíveis/pesquisáveis')
+            await message_obj.respond('Tenha em mente que pdfs grandes podem demorar para serem processados. Se já possuirem algum OCR, vai demorar um pouco mais, pois será necessário limpar o OCR anterior.')
+            await message_obj.respond('Para definir a(s) língua(s) do documento, utilize o comando -l lang1+lang2+lang3 no texto da mensagem do documento')
+            await message_obj.respond('No momento, são suportas as línguas português (por), inglês (eng) e espanhol (spa)')
+            await message_obj.respond('Exemplo de comando: `-l por+eng`')
+            await message_obj.respond('O código fonte pode ser encontrado em https://github.com/LuisF3')
+            await message_obj.respond('Se quiser, você pode doar uma quantia pelo pix. A chave aleatória é: 5edf6e87-8c5b-4cb9-b584-6ec1f12c8cbe')
 
 
-        @client.on(events.NewMessage(incoming=True, pattern='(^-)|(^$)', func=lambda e: e.message.file is not None))
+        @client.on(events.NewMessage(incoming=True, pattern='(^-)|(^$)', func=lambda e: e.file is not None and e.file.ext == '.pdf'))
         async def pdf_to_ocr(event: events.newmessage.NewMessage.Event):
             """Handles messages for applying ocr to a pdf
             
             This function handles incoming new messages that respects the 
             pattern '(^-)|(^$)' (messages that are empty or starts with -)
-            and have an attached file.
+            and have an attached pdf file.
 
             Args:
                 event (`events.newmessage.NewMessage.Event`):
                     The new message event (from telethon)
             """
 
+            message_obj: custom.message.Message = event.message
+
             log.info('pdf_to_ocr called')
-            await event.reply('Arquivo recebido!')
+            await message_obj.reply('Arquivo recebido!')
 
             with tempfile.TemporaryDirectory() as path:
                 default_args = {
-                    'input_file': f'{path}/{nanoid.generate()}_original.pdf',
-                    'output_file': f'{path}/{nanoid.generate()}_ocr.pdf',
+                    'input_file': None,
+                    'output_file': message_obj.file.name,
                     'language': ['por'],
                     'deskew': True,
                     'rotate_pages': True,
@@ -104,17 +107,16 @@ def main():
                     'progress_bar': False
                 }
 
-                langs = get_flags(event.message.message, '-l', '+')
+                langs = get_flags(message_obj.message, '-l', '+')
                 if len(langs) > 0:
                     log.info('Language set to: ' + ' '.join(langs))
                     default_args['language'] = langs
 
-                default_args['input_file'] = await event.download_media(file = default_args['input_file'])
-
+                default_args['input_file'] = await message_obj.download_media()
 
                 loop = asyncio.get_event_loop()
                 log.info('Iniciando processamento OCR')
-                await event.respond('Iniciando processamento OCR!')
+                await message_obj.respond('Iniciando processamento OCR!')
                 try:
                     await loop.run_in_executor(None, lambda : ocrmypdf.ocr(**default_args))
                 except ocrmypdf.PriorOcrFoundError:
@@ -126,16 +128,16 @@ def main():
                     await loop.run_in_executor(None, lambda : ocrmypdf.ocr(**default_args))
                 except ocrmypdf.MissingDependencyError as mde:
                     log.error('Não foi possível processar alguma das línguas solicitadas', mde)
-                    await event.reply('Não foi possível processar alguma das línguas solicitadas')
+                    await message_obj.reply('Não foi possível processar alguma das línguas solicitadas')
                     raise mde
                 except Exception as e:
                     log.error('Ocorreu um erro desconhecido', e)
                     raise e
 
 
-                await event.respond('OCR feito! Estamos fazendo upload do seu arquivo')
+                await message_obj.respond('OCR feito! Estamos fazendo upload do seu arquivo')
                 with open(default_args['output_file'], 'rb') as file:
-                    await event.reply('Aqui está!', file=file)
+                    await message_obj.reply('Aqui está!', file=file)
             log.info('Finalizado')
 
         log.info('Bot initiated')
