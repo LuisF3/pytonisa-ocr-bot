d127ed367ffb64b50bc1b046fb20cef6d1b33e70
Luís Chaves <pi.luis.f3@usp.br>
2021-08-11 15:06:09 -0300

    Structure for implementing aio-pika developed

--- ocr-bot.py
+++ ocr-bot.py
@@ -1,4 +1,6 @@
 import os
+import asyncio
+
 from logs import log
 from handlers import start_command, help_lang_command, more_info_command, pdf_to_ocr 
 
@@ -9,21 +11,44 @@ api_id = int(os.getenv('TELEGRAM_API_ID'))
 api_hash = os.getenv('TELEGRAM_API_HASH')
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 
-def main():
-    with TelegramClient('name', api_id, api_hash).start(bot_token=bot_token) as client:
-        client: TelegramClient
+async def start_telethon():
+    client: TelegramClient = await TelegramClient('name', api_id, api_hash).start(bot_token=bot_token)
 
-        client.add_event_handler(start_command, NewMessage(incoming=True, pattern='/start'))
-        client.add_event_handler(help_lang_command, NewMessage(incoming=True, pattern='/help_lang'))
-        client.add_event_handler(more_info_command, NewMessage(incoming=True, pattern='/more_info'))
-        client.add_event_handler(pdf_to_ocr, NewMessage(incoming=True, pattern='(^-)|(^$)', func=lambda e: e.file is not None and e.file.ext == '.pdf'))
+    client.add_event_handler(start_command, NewMessage(incoming=True, pattern='/start'))
+    client.add_event_handler(help_lang_command, NewMessage(incoming=True, pattern='/help_lang'))
+    client.add_event_handler(more_info_command, NewMessage(incoming=True, pattern='/more_info'))
+    client.add_event_handler(pdf_to_ocr, NewMessage(incoming=True, pattern='(^-)|(^$)', func=lambda e: e.file is not None and e.file.ext == '.pdf'))
 
-        log.info('Bot initiated')
-        client.run_until_disconnected()
+    telethon = client
+    return telethon
+
+def exit_telethon(telethon):
+    client = telethon
+    client.disconnect()
+
+async def start_rabbitmq():
+    return 5
+
+def exit_rabbitmq(rabbitmq):
+    print(rabbitmq)
+
+
+def main(loop: asyncio.AbstractEventLoop):
+    telethon = loop.run_until_complete(start_telethon())
+    rabbitmq = loop.run_until_complete(start_rabbitmq())
+
+    log.info('Bot initiated')
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        exit_telethon(telethon)
+        exit_rabbitmq(rabbitmq)
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
     try:
-        main()
+        main(loop)
     except:
         print('entrou')
         log.exception('Uncaugh exception...')
